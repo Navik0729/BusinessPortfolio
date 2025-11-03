8a32e2ffef9bc4b37b5304d0b30978e866579523
Navik0729 <navik0729@gmail.com>
2025-11-03 21:18:16 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-
-
-
-
 function Shuffle()
 {
-    
+ 
     var Ran = Math.round(Math.random()*5);
     var Ran2 = Math.round(Math.random()*5);
     var Ran3 = Math.round(Math.random()*5);
     
-
     const src = ["2025-10-27 (3).png", "2025-10-27 (4).png", "2025-10-27 (5).png", "2025-10-27 (6).png", "2025-10-27 (7).png", "2025-10-27 (8).png"];
     const alt = [ "Image of a business card design", "Image of a flyer design", "Image of a poster design", "Image of a brochure design", "Image of a banner design", "Image of a custom print design"];
         if(Ran == Ran2 || Ran == Ran3 || Ran2 == Ran3)
@@ -27,9 +22,9 @@ function Shuffle()
                 console.log("Greater value than 5.")
             }
         }
-    x = document.getElementById("SlideShow1");
-    y = document.getElementById("SlideShow2");
-    z = document.getElementById("SlideShow3");
+    x = document.getElementById("SS1");
+    y = document.getElementById("SS2");
+    z = document.getElementById("SS3");
 
   /// Assigning sources and alt texts based on random numbers
     
